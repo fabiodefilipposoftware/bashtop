7dac404aa6c1edf5f81558fcabccc67825a337ec
FabioDefilippo <fabiodefilipposoftware@gmail.com>
2026-06-09 23:30:22 +0200

    Update bashtop.sh

--- bashtop.sh
+++ bashtop.sh
@@ -1,9 +1,16 @@
-for file in *; do
-    if [ -f "$file" ]; then
-        # Ottieni il percorso assoluto del file
-        absolute_path="$(pwd)/$file"
-        
-        # clickable link filename
-        printf "\e]8;;file://%s\e\\%s\e]8;;\e\\\n" "$absolute_path" "$file"
-    fi
-done
+#!/bin/bash
+
+# Create a new session tmux called 'desktop' in background
+tmux new-session -d -s desktop
+
+# Run xstarter in the first tab (on left)
+tmux send-keys -t desktop:0.0 'xstarter' C-m
+
+# Split the window vertically in two tabs
+tmux split-window -h -t desktop:0.0
+
+# Run ranger in the second tab (on right)
+tmux send-keys -t desktop:0.1 'ranger' C-m
+
+# Connect the terminal session corrente
+tmux attach-session -t desktop
